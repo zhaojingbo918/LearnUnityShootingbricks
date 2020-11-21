67a3d435c8ad9e32e9ce08724cc56472e4fe6400
赵静波 <zhaojingbo918@163.com>
2020-11-21 16:13:51 +0800

    初步完成

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public int Speed = 3;
+
+    public GameObject Bullet;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("shoot Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  Debug.Log("shoot update");
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("shoot update mousebuttondown");
+            var temp = GameObject.Instantiate(Bullet, transform);
+            var rigidbody = temp.GetComponent<Rigidbody>();
+            var velocity = transform.forward * Speed;
+            rigidbody.velocity = velocity;
+
+            Debug.Log(string.Format("速度：{0}", velocity.ToString()));
+        }
+    }
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(h, v, 0) * Time.deltaTime);
+    }
+}
